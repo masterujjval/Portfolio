d55c00e1e596caddcdb018b62b57f13aed727c76
masterujjval <guptarickey3@gmail.com>
2024-07-12 23:15:52 +0530

    comimt

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,11 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-<<<<<<< HEAD
-import pdf from "../../Assets/../Assets/Ujjwal_Gupta.pdf";
-=======
 import pdf from "../../Assets/../Assets/Ujjwal-Gupta.pdf";
->>>>>>> c8749d9 (files)
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
